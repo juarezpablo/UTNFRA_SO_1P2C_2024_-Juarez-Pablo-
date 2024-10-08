fdfa104719478acc0a82f00882387dffe11f9d72
root <root@VMPruebas2>
2024-10-08 01:54:12 +0000

    Add: Punto_E.sh

--- RTA_SCRIPT_Examen_20241007/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_E.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 
-
-grep "MemTotal" /proc/meminfo > Filtro_Basico.txt
-dmidecode -t chassis | grep -i -e chassis -e manufacturer >> Filtro_Basico.txt
+grep "MemTotal" /proc/meminfo > $HOME/repogit/UTNFRA_SO_1P2C_2024_-Juarez-Pablo-/RTA_ARCHIVOS_Examen_20241007/Filtro_Basico.txt
+dmidecode -t chassis | grep -i -e chassis -e manufacturer >> $HOME/repogit/UTNFRA_SO_1P2C_2024_-Juarez-Pablo-/RTA_ARCHIVOS_Examen_20241007/Filtro_Basico.txt
 
 
 
